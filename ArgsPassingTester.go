58e1e1e0743595edeedd37f38aa1a8755c5232e0
Jan-wee Sha <janwee_sha@outlook.com>
2020-07-29 16:29:36 +0800

    a function arguments passing tester

--- ArgsPassingTester.go
+++ ArgsPassingTester.go
@@ -17,26 +17,24 @@ func main() {
 	fmt.Printf("address of int : %p\n", &i)
 	fmt.Printf("value of int : %d\n",i)
 
+	fmt.Println("---------------------------------------")
+
 	fmt.Printf("address of nums : %p\n", &nums)
-	var str string="["
+	var str string
 	for i,num := range nums{
 		str+=strconv.Itoa(num)
 		if i< len(nums)-1{
 			str+=","
-		}else{
-			str+="]"
 		}
 	}
 	fmt.Println("value of int array :"+str)
 	updatePassingArr(nums)
 	fmt.Printf("address of nums : %p\n", &nums)
-	var str2 string="["
+	var str2 string
 	for i,num := range nums{
 		str2+=strconv.Itoa(num)
 		if i< len(nums)-1{
 			str2+=","
-		}else{
-			str2+="]"
 		}
 	}
 	fmt.Println("value of int array :"+str2)
@@ -45,13 +43,11 @@ func main() {
 func updatePassingArr(nums []int) {
 	fmt.Printf("address of int array : %p\n", &nums)
 	nums[1] = 3
-	var str string="["
+	var str string
 	for i,num := range nums{
 		str+=strconv.Itoa(num)
 		if i< len(nums)-1{
 			str+=","
-		}else{
-			str+="]"
 		}
 	}
 	fmt.Println("value of int array :"+str)
