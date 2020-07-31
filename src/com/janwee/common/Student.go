3e836e9e0937d14f073cbd1dad00d4f25efe321e
Jan-wee Sha <janwee_sha@outlook.com>
2020-07-31 15:45:15 +0800

    error handling

--- src/com/janwee/common/Student.go
+++ src/com/janwee/common/Student.go
@@ -1,5 +1,7 @@
 package common
 
+import "errors"
+
 type Student struct {
 	name string
 	scoreMap map[string]float64
@@ -14,12 +16,15 @@ func (s *Student) GetName() string {
 	return s.name
 }
 
-func NewStudent (name string,scoreMap map[string]float64,rank int) *Student {
+func NewStudent (name string,scoreMap map[string]float64,rank int) (*Student,error) {
 	var totalScore float64
 	var averageScore float64
 	var passed bool=true
 	for course := range scoreMap {
 		score := scoreMap[course]
+		if score<0||score>100 {
+			return nil,errors.New("score should in range [0,100]")
+		}
 		totalScore+=score
 		passed = passed && score>=60
 	}
@@ -31,5 +36,5 @@ func NewStudent (name string,scoreMap map[string]float64,rank int) *Student {
 		totalScore: totalScore,
 		averageScore: averageScore,
 		passed: passed,
-	}
+	},nil
 }
--- src/com/janwee/main/TypeTester.go
+++ src/com/janwee/main/TypeTester.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"../common"
+	"fmt"
 )
 
 func main() {
-	s1 := common.NewStudent("Janwee",map[string]float64{common.Chinese: 65.5,common.Math:75,common.English:89},
+	s1,err := common.NewStudent("Janwee",map[string]float64{common.Chinese: 101,common.Math:75,common.English:89},
 	23)
-	common.PrintScore(s1)
+	if err != nil{
+		fmt.Println("error occurred while creating student")
+	}else{
+		common.PrintScore(s1)
+	}
+
 }
